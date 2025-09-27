e399cf656dcfac58d0b6e21c6e659eff4d3a4543
divyanshpatwari <divyanshpatwari36@gmail.com>
2025-09-27 23:20:12 +0530

    Initial commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pyspark.sql.window import *
 
 python_path = sys.executable
 os.environ['PYSPARK_PYTHON'] = python_path
-os.environ['JAVA_HOME'] = r'C:\Users\Computername\.jdks\corretto-1.8.0_462'
+os.environ['JAVA_HOME'] = r'C:\Users\Quantumn\.jdks\corretto-1.8.0_462'
 
 
 conf = SparkConf().setAppName("pyspark").setMaster("local[*]").set("spark.driver.host","localhost").set("spark.default.parallelism", "1")
@@ -19,16 +19,19 @@ spark = SparkSession.builder.getOrCreate()
 print("====================== pyspark started ==================")
 print()
 
+# Simple read first batch data and add respective columns ( )
 ###################  FIRST BATCH #######################
 wn = Window.orderBy("custid")
-df = (spark.read.csv("batch1.csv",sep=",",header="true")
-      .withColumn("deleteind",lit("0"))
-      .withColumn("incid",row_number().over(wn))
-      .withColumn("batchid",lit(0))
-      )
-df.show()
+day1df = (spark.read.csv("batch1.csv", sep=",", header="true")
+          .withColumn("deleteind",lit("0"))
+          .withColumn("incid",row_number().over(wn))
+          .withColumn("batchid",lit(0))
+          )
+day1df.show()
+
 
 ################### SECOND BATCH ##########################
+# Read second batch data and add deleteind column with all values as 0 initially
 
 df2 = (spark.read.csv("batch2.csv",sep=",",header="true")
        .select("custid","permanentAddress","temporaryAddress","products")
@@ -36,24 +39,51 @@ df2 = (spark.read.csv("batch2.csv",sep=",",header="true")
        )
 df2.show()
 
-joindf = (df.join(df2,"custid","left_anti").drop("incid","batchid")
+# Perform a left_anti join with yesterday's final data to get deleted records
+joindf = (day1df.join(df2, "custid", "left_anti").drop("incid", "batchid")
           .withColumn("deleteind",lit(1))
           .union(df2)
           )
 joindf.show()
 
-maxincid = df.select(max("incid")).collect()[0][0]  # Fetch yesterday's max increment id
-#print(maxincid)
-maxbatchid = df.select(max("batchid")).collect()[0][0] # Fetch yesterday's batch id
-#print(maxbatchid)
+maxincid = day1df.select(max("incid")).collect()[0][0]  # Fetch yesterday's max increment id (.collect()[0][0] will collect the dataframe value as string for further use)
+#print(maxincid) # To verify
+maxbatchid = day1df.select(max("batchid")).collect()[0][0] # Fetch yesterday's batch id
+#print(maxbatchid) # To verify
 
-# Number today's batch data with correct incid and batchid using yesterday's maxincid and maxbatchid
+# Add incid and batchid to today's batch data incrementally using yesterday's maxincid and maxbatchid
 
-fdf = joindf.rdd.zipWithIndex().map(
+day2df = joindf.rdd.zipWithIndex().map(
     lambda row_index:row_index[0] + (row_index[1] + maxincid+1,)
 ).toDF(joindf.columns + ["incid"]).withColumn("batchid",lit(maxbatchid+1))
-fdf.show()
+day2df.show()
 
-#fdf.write.format(sql/snowflake/s3).mode("append").save(targetlocation)
+
+####################### THIRD BATCH ##########################
+
+df3 = (spark.read.csv("batch3.csv", sep=",", header="true")
+       .select("custid","permanentAddress","temporaryAddress","products")
+       .withColumn("deleteind",lit(0))
+       )
+df3.show()
+
+joindf2 = (day2df.join(df3,"custid","left_anti").drop("incid","batchid")
+           .withColumn("deleteind",lit(1))
+           .union(df3)
+           )
+joindf2.show()
+
+day2maxincid = day2df.select(max("incid")).collect()[0][0]
+print(day2maxincid)
+day2maxbatchid = day2df.select(max("batchid")).collect()[0][0]
+print(day2maxbatchid)
+
+day3df = joindf2.rdd.zipWithIndex().map(
+    lambda row_index:row_index[0] + (row_index[1] + day2maxincid+1,)
+).toDF(joindf2.columns + ["incid"]).withColumn("batchid",lit(day2maxbatchid + 1))
+day3df.show()
+
+
+#fdf.write.format(local/sql/snowflake/s3).mode("append").save(targetlocation)
 
 
